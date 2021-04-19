2ade7c5661d298199e7ebb0d3d5aad31fb097069
Филип Арсовски <arshetamine@protonmail.com>
2021-04-20 01:14:12 +0200

    Fixing the main loop.

--- crypto-telegram.py
+++ crypto-telegram.py
@@ -6,52 +6,53 @@ import math
 
 # Load environment (.env) settings.
 load_dotenv()
-EXCHANGE_RATES_URL = os.getenv('EXCHANGE_RATES_URL')
-TELEGRAM_BOT_API = os.getenv('TELEGRAM_BOT_API')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+EXCHANGE_RATES_URL = os.getenv("EXCHANGE_RATES_URL")
+TELEGRAM_BOT_API = os.getenv("TELEGRAM_BOT_API")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 # Threshold settings.
 threshold = {
-    'BTC': {'MAX': 0.00, 'MIN': 0.00},
-    'ETH': {'MAX': 0.00, 'MIN': 0.00},
-    'ALGO': {'MAX': 0.00, 'MIN': 0.00},
+    "bitcoin": {"MAX": 0.00, "MIN": 0.00},
+    "ethereum": {"MAX": 0.00, "MIN": 0.00},
+    "algorand": {"MAX": 0.00, "MIN": 0.00},
 }
 
-history = {'BTC': [], 'ETH': [], 'ALGO': []}
+history = {"counter": 0, "bitcoin": [], "ethereum": [], "algorand": []}
 
-delay = 60 * 5
+time_interval = 60 * 10
 
 
 def sendMessage(message):
-    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_API}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&parse_mode=html&text={message}'
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_API}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&parse_mode=html&text={message}"
     requests.get(url)
 
 
 def getValue(coin, fiat):
-    url = f'{EXCHANGE_RATES_URL}{coin}-{fiat}/spot'
+    url = f"{EXCHANGE_RATES_URL}ids={coin}&vs_currencies={fiat}"
     response = requests.get(url)
     response_json = response.json()
-    price = float(response_json['data']['amount'])
+    price = float(response_json[coin][fiat])
     return price
+
 
 # -----------------------------------------------------------------
 
 
 def updateThreshold(coin, type, value):
-    if coin == 'BTC':
-        if type == 'MAX':
+    if coin == "bitcoin":
+        if type == "MAX":
             threshold[coin][type] = value + 500.00
-        elif type == 'MIN':
+        elif type == "MIN":
             threshold[coin][type] = value - 500.00
-    elif coin == 'ETH':
-        if type == 'MAX':
+    elif coin == "ethereum":
+        if type == "MAX":
             threshold[coin][type] = value + 100.00
-        elif type == 'MIN':
+        elif type == "MIN":
             threshold[coin][type] = value - 100.00
-    elif coin == 'ALGO':
-        if type == 'MAX':
+    elif coin == "algorand":
+        if type == "MAX":
             threshold[coin][type] = value + 0.05
-        elif type == 'MIN':
+        elif type == "MIN":
             threshold[coin][type] = value - 0.05
 
 
@@ -60,10 +61,6 @@ def calibrateThreshold(coin, fiat):
     price = round(price, 2)
     updateThreshold(coin, "MAX", price)
     updateThreshold(coin, "MIN", price)
-    max = round(threshold[coin]['MAX'], 2)
-    min = round(threshold[coin]['MIN'], 2)
-    sendMessage(
-        f'🔧 <b>{coin}</b> CALIBRATING: <code>{price}</code> <i>{fiat}</i>\n<i>calibrated threshold - MAX: {max} - MIN: {min} {fiat}</i>')
 
 
 def checkCoin(coin, fiat):
@@ -76,33 +73,38 @@ def checkCoin(coin, fiat):
     if price >= threshold_max:
         calibrateThreshold(coin, fiat)
         sendMessage(
-            f'⚠️ <b>{coin}</b> Rise Signal: <code>{price}</code> <i>{fiat}</i> 📈\n<i>current threshold - {round(threshold_max, 2)} {fiat}</i>\n[AI] <i>calibrated threshold - {round(threshold[coin]["MAX"], 2)} {fiat}</i>')
+            f'​⬆️​ <b>{coin.upper()}</b> Rise Signal: <code>{price}</code> <i>{fiat.upper()}</i> 📈\n<i>current threshold - {round(threshold_max, 2)} {fiat.upper()}</i>\n[AI] <i>calibrated threshold - {round(threshold[coin]["MAX"], 2)} {fiat.upper()}</i>'
+        )
 
     elif price <= threshold_min:
         calibrateThreshold(coin, fiat)
         sendMessage(
-            f'⚠️ <b>{coin}</b> Down Signal: <code>{price}</code> <i>{fiat}</i> 📉\n<i>current threshold - {round(threshold_min, 2)} {fiat}</i>\n[AI] <i>calibrated threshold - {round(threshold[coin]["MIN"], 2)} {fiat}</i>')
+            f'⬇️ <b>{coin.upper()}</b> Down Signal: <code>{price}</code> <i>{fiat.upper()}</i> 📉\n<i>current threshold - {round(threshold_min, 2)} {fiat.upper()}</i>\n[AI] <i>calibrated threshold - {round(threshold[coin]["MIN"], 2)} {fiat.upper()}</i>'
+        )
 
 
-def checkAll():
-    checkCoin('BTC', 'EUR')
-    checkCoin('ETH', 'EUR')
-    checkCoin('ALGO', 'EUR')
-    print("Checked all.")
-    time.sleep(delay)
-
-
-def startChecker():
-    while True:
-        checkAll()
-
-
-sendMessage(
-    f'<b>Crypto-Telegram</b> \n<code>[by arshetamine with love.]</code>')
+sendMessage(f"<b>Crypto-Telegram</b> \n<code>[by arshetamine with love.]</code>")
 
 time.sleep(5)
-calibrateThreshold('BTC', 'EUR')
-calibrateThreshold('ETH', 'EUR')
-calibrateThreshold('ALGO', 'EUR')
 
-startChecker()
+calibrateThreshold("bitcoin", "eur")
+calibrateThreshold("ethereum", "eur")
+calibrateThreshold("algorand", "eur")
+
+
+# -----------------------------------------------------------------
+# Main Loop
+
+
+def main():
+    while True:
+        print("Getting DATA...")
+        checkCoin("bitcoin", "eur")
+        checkCoin("ethereum", "eur")
+        checkCoin("algorand", "eur")
+
+        time.sleep(time_interval)
+
+
+if __name__ == "__main__":
+    main()
